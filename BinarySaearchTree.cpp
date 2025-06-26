9df91d3216ad0615ff3e7d6eb76f337ee8a8b800
Haffi24 <haffisaefulloh@gmail.com>
2025-06-26 10:54:27 +0700

    menambahkan konstruktor node classs

--- BinarySaearchTree.cpp
+++ BinarySaearchTree.cpp
@@ -4,4 +4,17 @@ using namespace std;
 class Node
 {
 public:
-   
+    int info;
+    Node *leftchild;
+    Node *rigtchild;
+
+
+    //Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; 
+        rigtchild = nullptr;
+    }
+};
+
+
